c0b779d82fc7ec8d271258687e8864967005c713
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-07-08 15:52:26 -0300

    add-new-functions-to-return-one-product-and-one-user

--- api/inc/api_database.php
+++ api/inc/api_database.php
@@ -1,11 +1,11 @@
 <?php
 
 require_once(dirname(__FILE__) . '/database.php');
+require_once(dirname(__FILE__) . '/config.php');
 
 class api_database extends Dao
 {
-    private $sql;
-
+    /*
     private function check_active_param($params, $param)
     {
         if (isset($params[$param])) {
@@ -25,7 +25,7 @@ class api_database extends Dao
         }
         return $this->sql;
     }
-
+    */
     // analisa um array de parâmetros
     /**
      * @param params -> array de parâmetros vindos da query string
@@ -33,15 +33,16 @@ class api_database extends Dao
      * @return SQLString
      * Se o @param 1 'status' e o @param 2 'texto', você consegue analisar usando essa função
      */
-    private function check_array_param($params, $param)
+    /*
+    private function check_array_param($arr, $param)
     {
         if (count($param) > 2) {
             die("Many arguments in function 'check_array_param'");
         }
         // se existir o parâmetro a ser analisado
-        if (isset($params[$param[0]])) {
+        if (isset($arr[$param[0]])) {
 
-            $analyzed_parameter = $params[$param[0]];
+            $analyzed_parameter = $arr[$param[0]];
 
             // verifica se os valores passados são diferentes de true ou false
             if (($analyzed_parameter != 'true' && $analyzed_parameter != 'false')) {
@@ -52,7 +53,7 @@ class api_database extends Dao
             $analyzed_parameter = ($analyzed_parameter == 'true' ? true : false);
 
             // serve para manipular strings
-            if (isset($params[$param[1]])) {
+            if (isset($arr[$param[1]])) {
 
                 if ($analyzed_parameter == true) {
                     $this->sql .= "AND $param[0] IS NOT NULL ";
@@ -74,41 +75,69 @@ class api_database extends Dao
 
         return $this->sql;
     }
+    */
 
-    public function get_all_users($params = null)
+    public function get_all_users()
     {
-        $this->sql = 'SELECT username AS user, email, role FROM Users ';
-
-        if ($this->check_active_param($params, 'active') == 'active') {
-            $result = 'active';
-            return $result;
-        }
-
-        if ($this->check_array_param($params, ['email', 'active']) == 'email') {
-            $result = 'email';
-            return $result;
-        }
-
+        $this->sql = BASE_QUERY_USERS;
         $result = $this->runQuery($this->sql);
         return $result;
     }
 
-    public function get_all_products($params = null)
+    public function get_user($id)
     {
-        $this->sql = "SELECT p.name, p.description, p.price, p.stock_quantity, c.name as categorie, status 
-                      FROM products p 
-                      INNER JOIN categories c ON c.id_categorie = p.id_categorie ";
+        $this->sql = BASE_QUERY_USERS . " WHERE id_user = $id";
+        $result = $this->runQuery($this->sql);
+        return $result;
+    }
 
-        if($this->check_active_param($params, 'status') == 'status'){
-            $result = 'status';
-            return $result;
-        }
+    public function get_all_active_users()
+    {
+        // return all active clients
+        $this->sql = BASE_QUERY_USERS . " WHERE status IS TRUE";
+        $result = $this->runQuery($this->sql);
+        return $result;
+    }
 
-        if($this->check_array_param($params, ['description', 'status']) == 'description'){
-            $result = 'description';
-            return $result;   
-        }
+    public function get_all_inactive_users()
+    {
+        // return all active clients
+        $this->sql = BASE_QUERY_USERS . " WHERE status IS FALSE";
+        $result = $this->runQuery($this->sql);
+        return $result;
+    }
 
+    public function get_all_products()
+    {
+        $this->sql = BASE_QUERY_PRODUCT;
+        $result = $this->runQuery($this->sql);
+        return $result;
+    }
+
+    public function get_product($id)
+    {
+        $this->sql = BASE_QUERY_PRODUCT . " WHERE id_product = $id";
+        $result = $this->runQuery($this->sql);
+        return $result;
+    }
+
+    public function get_all_active_products()
+    {
+        $this->sql = BASE_QUERY_PRODUCT . " WHERE status IS TRUE";
+        $result = $this->runQuery($this->sql);
+        return $result;
+    }
+
+    public function get_all_inactive_products()
+    {
+        $this->sql = BASE_QUERY_PRODUCT . " WHERE status IS FALSE";
+        $result = $this->runQuery($this->sql);
+        return $result;
+    }
+
+    public function get_all_clients()
+    {
+        $this->sql = BASE_QUERY_CLIENTS;
         $result = $this->runQuery($this->sql);
         return $result;
     }
