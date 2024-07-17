942dd945b725080c27ef995d7ea3d9a54bb324b1
jefferson oliveira <jefferson.oliveira203@etec.sp.gov.br>
2024-07-17 16:00:19 -0300

    algorithm-romanToInt

--- index.php
+++ index.php
@@ -27,6 +27,48 @@
     </style>
 
 <body>
+<?php
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function romanToInt($s) {
+        $result = 0;
+
+        $data = [
+            "I" => 1,
+            "V" => 5,
+            "X" => 10,
+            "L" => 50,
+            "C" => 100,
+            "D" => 500,
+            "M" => 1000
+        ];
+
+        if($s == null || $s < 1)
+        {
+            return -1;
+        }
+
+        for($i = 0; $i < strlen($s); $i++)
+        {
+            if($s[$i] === "I" && $s[$i+1] === "V")
+            {
+                $result += 4;
+                $i++;
+            } else if($s[$i] === "I" && $s[$i+1] === "X")
+            {
+                $result += 9;
+                $i++;
+            }
+        }
+    }
+}
+
+?>
 
 </body>
 </html>
